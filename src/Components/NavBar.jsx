d748123c0636d480b2f614f81ed90038ff077be0
Saheer k <143588271+saheer-dev@users.noreply.github.com>
2023-09-18 16:13:54 +0530

    fixed mobile scrollbar

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -13,7 +13,7 @@ let count = useContext(cartValue)
 
         <div className='flex items-center justify-center gap-3 md:pr-5 '>
             <p className='text-lg'>My Orders</p>
-            <div className='relative w-4 h-4'>
+            <div className='relative w-7 h-7 items-center justify-center flex'>
               <HiShoppingCart size={28} className=''/>
                  <p className='w-4 h-4 absolute left-4 -top-1 md:-top-1 md:left-4  grid place-items-center text-[0.6rem] bg-red-600 text-white rounded-full'>{count.cart}</p>
 
